fa3663e0f5ebd0e39cab7e163c71cc8633577165
krutika bhadane <krutikabhadane79@gmail.com>
2024-11-15 16:02:01 +0530

    Added: UC-2 Calculate emp wage

--- src/main/java/com/bridgelabz/empwage/EmpWage.java
+++ src/main/java/com/bridgelabz/empwage/EmpWage.java
@@ -6,14 +6,19 @@ public class EmpWage
     {
         System.out.println( "Welcome to empwage" );
         final int FULL_TIME = 1;
+        int WAGE_PER_HR = 20;
+        int workingHours = 0;
         double empType = Math.floor(Math.random() * 10) % 2;
         if (empType == FULL_TIME)
         {
             System.out.println("Employee is Present");
+            workingHours = 8;
         }
         else
         {
             System.out.println("Employee is Absent");
         }
+        int wage = workingHours * WAGE_PER_HR;
+        System.out.println("Employee Daily Wage is " + wage);
     }
 }
