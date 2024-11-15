4998dc35c37bea109cf22db1da4955ffece24598
krutika bhadane <krutikabhadane79@gmail.com>
2024-11-15 15:58:17 +0530

    Added: UC-1 Check emp is present or absent

--- src/main/java/com/bridgelabz/empwage/EmpWage.java
+++ src/main/java/com/bridgelabz/empwage/EmpWage.java
@@ -5,5 +5,15 @@ public class EmpWage
     public static void main( String[] args )
     {
         System.out.println( "Welcome to empwage" );
+        final int FULL_TIME = 1;
+        double empType = Math.floor(Math.random() * 10) % 2;
+        if (empType == FULL_TIME)
+        {
+            System.out.println("Employee is Present");
+        }
+        else
+        {
+            System.out.println("Employee is Absent");
+        }
     }
 }
