c3d50785ca6e12d6ec72e401e0c64daca1b3e715
krutikabhadane79 <krutikabhadane79@gmail.com>
2024-11-23 14:49:04 +0530

    Added: UC2 Calculate the employee wage

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -4,14 +4,19 @@ public class EmpWage {
     public static void main(String[] args) {
         System.out.printf("Welcome to employee wage");
         final int FULL_TIME = 1;
+        int WAGE_PER_HR = 20;
+        int workingHours = 0;
         double empType = Math.floor(Math.random() * 10) % 2;
         if (empType == FULL_TIME)
         {
             System.out.println("Employee is Present");
+            workingHours = 8;
         }
         else
         {
             System.out.println("Employee is Absent");
         }
+        int wage = workingHours * WAGE_PER_HR;
+        System.out.println("Employee Daily Wage is " + wage);
     }
 }
