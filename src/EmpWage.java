91b8e2425f1941edb7b7aafe4cc37f73afde03af
krutikabhadane79 <krutikabhadane79@gmail.com>
2024-11-23 14:43:39 +0530

    Added: UC1 Check employee is presnt or absent

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -3,6 +3,15 @@
 public class EmpWage {
     public static void main(String[] args) {
         System.out.printf("Welcome to employee wage");
-
+        final int FULL_TIME = 1;
+        double empType = Math.floor(Math.random() * 10) % 2;
+        if (empType == FULL_TIME)
+        {
+            System.out.println("Employee is Present");
+        }
+        else
+        {
+            System.out.println("Employee is Absent");
+        }
     }
 }
